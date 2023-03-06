4072095fb221a1aa7ed5495acef9129944707be1
ITokianaR <tokina.raman@gmail.com>
2023-03-06 21:05:07 +0300

    adding dark mode feature

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
-    return(<footer class="p-4 bg-black shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-black">
-            <span class="text-sm text-white sm:text-center dark:text-white">© 2023 <a href="https://flowbite.com/" class="hover:underline">FlexCorp™</a>. All Rights Reserved.
+    return(<footer class="p-4 bg-black shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-white">
+            <span class="text-sm text-white sm:text-center dark:text-black">© 2023 <a href="https://flowbite.com/" class="hover:underline">FlexCorp™</a>. All Rights Reserved.
             </span>
-            <ul class="flex flex-wrap items-center mt-3 text-sm text-white dark:text-white sm:mt-0">
+            <ul class="flex flex-wrap items-center mt-3 text-sm text-white dark:text-black sm:mt-0">
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
                 </li>
                 <li>
                     <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
