6b46b4c9928a82ba19b525fb894f380d4092a8de
ITokianaR <tokina.raman@gmail.com>
2023-03-08 18:23:21 +0300

    terminating students page

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,5 +1,6 @@
 export default function Footer() {
-    return(<footer class="p-4 bg-black shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-white">
+    return(
+    <footer class="p-4 sticky bottom-0 bg-black shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-300">
             <span class="text-sm text-white sm:text-center dark:text-black">© 2023 <a href="https://flowbite.com/" class="hover:underline">FlexCorp™</a>. All Rights Reserved.
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm text-white dark:text-black sm:mt-0">
