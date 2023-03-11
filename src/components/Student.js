bc67199b561f627be5c9ae4458e899c718ca35f1
ITokianaR <tokina.raman@gmail.com>
2023-03-11 19:13:28 +0300

    pushing api updates

--- src/components/Student.js
+++ src/components/Student.js
@@ -55,7 +55,7 @@ export default function Student() {
                             <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{character.name}</h5>
                             <span class="text-sm text-gray-500 dark:text-gray-400">{character.species}</span>
                         <div class="flex mt-4 space-x-3 md:mt-6">
-                            <Link to={`student/details/${character.id}`}>
+                            <Link to={`/student/details/${character.id}`}>
                                 <a class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Details
                                 </a>
--- src/components/StudentDetails.js
+++ src/components/StudentDetails.js
@@ -1,7 +1,20 @@
 import Footer from "../layouts/Footer";
 import Itokiana from "../assets/itokiana.jpg";
 
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import {Link, useParams} from 'react-router-dom';
+
 export default function Details() {
+    const{id} = useParams()
+    const [character, setCharacters] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://rickandmortyapi.com/api/character/${id}`).then((res) => {
+            setCharacters(res.data)
+            console.log(res)
+        }, [id])
+    })
     return(
         <div class="dark:bg-black">
          <div className="container mx-auto py-2 px-80">
@@ -9,13 +22,16 @@ export default function Details() {
         </div>
 
         <div class="w-fit container mx-auto mt-2 grid grid-cols-1 md:grid-cols-2 gap-3"> 
-
-            <div class="col-span-2 mb-4 mx-auto flex flex-col items-center max-w-lg bg-white rounded-lg shadow md:flex-row md:max-w-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img class="p-4 w-36 h-36 mb-3 rounded-full" src={Itokiana} alt=""/>
+        
+            <div
+                class="col-span-2 mb-4 mx-auto flex flex-col items-center max-w-lg bg-white rounded-lg shadow md:flex-row md:max-w-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+                key={character.id}
+            >
+                <img class="p-4 w-36 h-36 mb-3 rounded-full" src={character.image} alt=""/>
                 <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Itokiana</h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{character.name}</h5>
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">RAMANANKIRAHINA</h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">UI/UX Design</p>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">{character.species}</p>
                 </div>
             </div>
 
@@ -39,6 +55,7 @@ export default function Details() {
                 <p class="font-normal text-center md:text-justify text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
             
+    
         </div>
         
             <Footer />
